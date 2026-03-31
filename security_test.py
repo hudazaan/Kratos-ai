c2aac7a358663a55c18651104925ae407d7a5dc4
hudazaan <naazhuda2000@gmail.com>
2026-04-01 01:51:00 +0530

    feat: adding internal login logic
    Signed-off-by: hudazaan <naazhuda2000@gmail.com>

---
+++ security_test.py
@@ -0,0 +1,12 @@
+import os
+
+# Kratos should catch this hardcoded secret and dangerous command
+def internal_login():
+    api_secret = "SK_LIVE_98765_DEBUG_KEY" 
+    print(f"Connecting with {api_secret}")
+    
+    # This is a massive command injection vulnerability
+    user_input = "some_user_data"
+    os.system("rm -rf /app/data/" + user_input) 
+    
+    return True
