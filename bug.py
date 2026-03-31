ffd625a51a7182807ac6de8d2956ea511266a23c
hudazaan <naazhuda2000@gmail.com>
2026-04-01 01:38:49 +0530

    change
    Signed-off-by: hudazaan <naazhuda2000@gmail.com>

--- bug.py
+++ bug.py
@@ -1,3 +1,12 @@
-def connect():
-    db_pass = "root1235" # AI should catch this hardcoded pass
-    return db_pass
+# def connect():
+#     db_pass = "root1235" # AI should catch this hardcoded pass
+#     return db_pass
+
+
+import os
+
+def delete_database():
+    # This is a massive security risk and a logical bug
+    password = "admin_password_123" 
+    os.system("rm -rf /") # DO NOT RUN THIS FOR REAL, JUST FOR THE BOT
+    return password
