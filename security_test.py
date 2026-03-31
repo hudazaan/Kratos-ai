5ab022a03da8c437348e3b1a2e7de27fd89eb7d2
hudazaan <naazhuda2000@gmail.com>
2026-04-01 01:57:29 +0530

    updated logic
    Signed-off-by: hudazaan <naazhuda2000@gmail.com>

--- security_test.py
+++ security_test.py
@@ -5,7 +5,7 @@ def internal_login():
     api_secret = "SK_LIVE_98765_DEBUG_KEY" 
     print(f"Connecting with {api_secret}")
     
-    # This is a massive command injection vulnerability
+    # This is a serious command injection vulnerability
     user_input = "some_user_data"
     os.system("rm -rf /app/data/" + user_input) 
     
